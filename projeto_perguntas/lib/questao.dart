3fed8c90558e1beaf993fb13bd1629788d32fe67
Eber <eberson.santoscosme@gmail.com>
2024-08-09 12:18:33 -0300

    Estilizando texto

---
+++ projeto_perguntas/lib/questao.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class Questao extends StatelessWidget {
+
+    final String texto;
+
+    const Questao(this.texto);
+
+    @override
+    Widget build(BuildContext context) {
+        return Container(
+            width: double.infinity, //usando o tamanho maximo do double
+            margin: EdgeInsets.all(10),
+            child: Text(
+                texto,
+                style: TextStyle(fontSize: 28), //Tamanho do texto
+                textAlign: TextAlign.center, //Alinhamento do texto
+          ),
+        );
+    }
+}
