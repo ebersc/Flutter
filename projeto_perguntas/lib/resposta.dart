eca79fc5a4f50bf5ab4d060b49e460731eae3f06
Eber <eberson.santoscosme@gmail.com>
2024-08-09 13:56:59 -0300

    Componente respostas

---
+++ projeto_perguntas/lib/resposta.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class Resposta extends StatelessWidget {
+
+    final String texto;
+
+    final void Function() quandoSelecionado;
+
+    const Resposta(this.texto, this.quandoSelecionado);
+
+    @override
+    Widget build(BuildContext context) {
+        return Container(
+            width: double.infinity,
+            child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                    foregroundColor: Colors.white, // foreground (text) color
+                    backgroundColor: Colors.blue, // background color
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(0),
+                    ),
+                ),
+                onPressed: () {},
+                child: Text(texto),
+            ),
+        );
+    }
+}
