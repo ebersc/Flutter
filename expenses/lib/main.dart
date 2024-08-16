74678bcc963beb109ebb3e82aa7de35ecf1a21eb
ebersc <eberson.santoscosme@gmail.com>
2024-08-16 17:52:40 -0300

    aplicando estilo no componente

--- expenses/lib/main.dart
+++ expenses/lib/main.dart
@@ -1,8 +1,10 @@
+import 'package:expenses/models/transaction.dart';
 import 'package:flutter/material.dart';
 
 main() => runApp(ExpensesApp());
 
 class ExpensesApp extends StatelessWidget {
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(home: MyHomePage());
@@ -10,7 +12,19 @@ class ExpensesApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key});
+
+  final _transactions = [
+    Transaction(
+        id: 't1',
+        title: 'Novo tênis de corrida',
+        value: 310.76,
+        date: DateTime.now()),
+    Transaction(
+        id: 't2',
+        title: 'Conta de Luz',
+        value: 211.30,
+        date: DateTime.now()),
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -29,8 +43,37 @@ class MyHomePage extends StatelessWidget {
                 elevation: 5,
               ),
             ),
-            Card(
-              child: Text('Lista de transações'),
+            Column(
+              children: _transactions.map((tr) {
+                return Card(
+                  child: Row(
+                    children: [
+                      Container(
+                        margin: EdgeInsets.symmetric(
+                          horizontal: 15,
+                          vertical: 10
+                        ),
+                        decoration: BoxDecoration(
+                          border: Border.all(
+                            color: Colors.black,
+                            width: 2,
+                          )
+                        ),
+                        padding: EdgeInsets.all(10),
+                        child: Text(
+                          tr.value.toString()
+                        ),
+                      ),
+                      Column(
+                        children: [
+                          Text(tr.title),
+                          Text(tr.date.toString())
+                        ],
+                      )
+                    ],
+                  ),
+                );
+              }).toList(),
             )
           ],
         ));
